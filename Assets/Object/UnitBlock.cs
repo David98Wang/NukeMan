22373ffb7794eda913d8ec51b87773d61c6d4264
David Wang <whcdavidca@gmail.com>
2018-01-22 02:41:23 -0500

    Implemented constructor for UnitBlock object.

--- Assets/Object/UnitBlock.cs
+++ Assets/Object/UnitBlock.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitBlock : MonoBehaviour {
+public class UnitBlock : MonoBehaviour{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public double Width = 5, Height =5;
+    enum BackgroundType
+    {
+        Picture = 0,
+        SolidColor = 1
+    };
+
+    public UnitBlock(float PositionX, float PositionY,float PositionZ)
+    {
+        SetX(PositionX);
+        SetY(PositionY);
+        SetZ(PositionZ);
+    }
+
+    public void Start()
+    {
+        
+    }
+    #region Private Function
+    private void SetX(float newX)
+    {
+        Vector3 temp = this.transform.position;
+        temp.x = newX;
+        this.transform.position = temp;
+    }
+    private void SetY(float newY)
+    {
+        Vector3 temp = this.transform.position;
+        temp.y = newY;
+        this.transform.position = temp;
+    }
+    private void SetZ(float newZ)
+    {
+        Vector3 temp = this.transform.position;
+        temp.z = newZ;
+        this.transform.position = temp;
+    }
+    #endregion
 }
